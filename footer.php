a4f15358e147bf46dd18f15ef2abd5320d759bab
Howard <s3610983@student.rmit.edu.au>
2019-04-20 17:37:21 +0800

    Change footer

--- footer.php
+++ footer.php
@@ -1,1 +1,4 @@
+<footer>
+  <h4>&copy;2019 Haolei Zhou,&nbspSherman Wong,&nbspMadhumitha Rajkumar,&nbspZhenni liu,&nbspXun li</h4>      
+</footer>
 </html>
--- index.php
+++ index.php
@@ -2,6 +2,5 @@
 <title>CoffeeBuzz</title>
 <body>
 	<p>This is the CoffeeBuzz system made specially for 4U at the University Campus.</p>
-	<p>Test 2</p>
 </body>
 <?php include_once('footer.php'); ?>
