f774a011e1e94725b481c4a013d32e72e9a6bdf1
Howard <s3610983@student.rmit.edu.au>
2019-04-20 15:55:58 +0800

    Test

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 <title>CoffeeBuzz</title>
 <body>
 	<p>This is the CoffeeBuzz system made specially for 4U at the University Campus.</p>
-	<p></p>
+	<p>Test</p>
 </body>
 <?php include_once('footer.php'); ?>
