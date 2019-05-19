85b1ea4150ced893abe0246cbce0223ab70a5363
Sherman Wong <s3656264@student.rmit.edu.au>
2019-05-19 12:25:29 +1000

    Clarifying code by adding tabs

--- queue.php
+++ queue.php
@@ -1,23 +1,22 @@
 <?php include_once('header.php'); ?>
 <title>Queue Info</title>
 <body>	
-<div class="center">
-<h1>Calculate time</h1>
-Order number:<br>
-<input id="ordernum" type="text" name="ordernum"><br>
-<button onclick="time()">Calculate</button>
-<p>Your waiting time is: <strong id="time"></strong>min</p>
-</div>
+	<div class="center">
+		<h1>Calculate time</h1>
+		Order number:<br>
+		<input id="ordernum" type="text" name="ordernum"><br>
+		<button onclick="time()">Calculate</button>
+		<p>Your waiting time is: <strong id="time"></strong>min</p>
+	</div>
 
-<script>
-function time()
-{
-var ordernum = document.getElementById("ordernum").value;
-var time = document.getElementById("time").value;
-var time = 5 * ordernum;
-document.getElementById("time").innerHTML = time.toFixed(2);
-}
-</script>
+	<script>
+	function time() {
+		var ordernum = document.getElementById("ordernum").value;
+		var time = document.getElementById("time").value;
+		var time = 5 * ordernum;
+		document.getElementById("time").innerHTML = time.toFixed(2);
+	}
+	</script>
 
 </body>
 <?php include_once('footer.php'); ?>
