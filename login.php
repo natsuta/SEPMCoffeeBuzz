b8e1945747c8ff58335c0f874aba3a3d5632ebd6
Howard <s3610983@student.rmit.edu.au>
2019-05-21 20:22:26 +0800

    login page change

--- login.php
+++ login.php
@@ -1,41 +1,106 @@
 <html>
-<head>
-<title> Login Page</title>
-    <link rel="stylesheet" type ="text/css" href ="style.css">
-</head>  
-    
-<body>
-    <div class="loginbox">
-    <div class="form">
-        <form class="registeration-form">
-        <input type="text" placeholder="user name"/>
-        <input type="password" placeholder="password"/>
-        <input type="text" placeholder="email id"/>
-        <button>Create </button>
-            <p class="message">Already our customer.? 
-            <a href="index.php">Login</a>
-            </p>
-        </form>
-        <img src ="avatar2.png" class="avatar"> 
-    
-        <form class="login-form">
-         <input type ="text"  placeholder="Enter username">
-         <input type ="password" name ="" placeholder="Enter Password">
-            <button ><a href="index.php">login</a></button><br>
-        <p class="message"> Not Registered? <a href="#"> Register</a></p>
-            <a href ="#"> Forgot password.?</a><br>
-        </form>
-    </div>
-    </div>
-    
-    <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+    <head>
+    <title> Login Page</title>
+        <link rel="stylesheet" type ="text/css" href ="style.css">
+    </head>  
 
-    <script>
-        $('.message a').click(function(){
-        $('form').animate({height: "toggle", opacity:"toggle"},"slow");
+    <body>
+         <div class="loginbox">
+        <div class="form">
+            <form class="registeration-form" name="RegForm" onsubmit="return validation()" method="post">
+            <input type="text" placeholder="user name" name = "Name"/>
+            <input type="password" placeholder="password" name = "Password"/>
+            <input type="text" placeholder="email id" name = "Email"/>
+            <button type="submit" value="create" name="Submit">Create</button>
+                <p class="message">Already our customer? 
+                <button type="button" id="formButton"><a href="index.php">Login</a></button>
+                </p>
+            </form>
+        <img src ="avatar2.png" class="avatar">
+             <form id="form1" name="loginForm" onsubmit="return validationLogin()" method="post">
+             <input type="text" placeholder="user name" name = "Name"/>
+            <input type="password" placeholder="password" name = "Password"/>
+            <button type="submit" value="submit" name="Submit">Submit</button>
+            </form>
+             </div>
+            </div>
+    </body>
+        <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+     <script>
+         $("#formButton").click(function(){
+            $("#form1").toggle(250);
         });
-                              
-    </script>
-</body>
- 
-</html>
+         function validation()
+         {
+            var name = document.forms["RegForm"]["Name"];
+            var email = document.forms["RegForm"]["Email"];
+            var password = document.forms["RegForm"]["Password"];
+
+             if (name.value == "")                                  
+        { 
+            
+            window.alert("Please enter your name."); 
+            name.focus(); 
+            return false; 
+        }
+
+             if (email.value == "")                                   
+        { 
+            window.alert("Please enter a valid e-mail address."); 
+            
+            email.focus(); 
+            return false; 
+        } 
+
+        if (email.value.indexOf("@", 0) < 0)                 
+        { 
+            window.alert("Please enter a valid e-mail address."); 
+            email.focus(); 
+            return false; 
+        } 
+
+        if (email.value.indexOf(".", 0) < 0)                 
+        { 
+            window.alert("Please enter a valid e-mail address."); 
+            email.focus(); 
+            return false; 
+        } 
+
+           if (password.value == "" && password.value.length < 8)                        
+        { 
+            window.alert("Please enter your password"); 
+            password.focus(); 
+            return false; 
+        }
+            if (password.value.length < 8)                        
+        { 
+            window.alert("Please enter atleast 8 character"); 
+            password.focus(); 
+            return false; 
+        }
+                 return true;
+         }
+         
+         function validationLogin()
+        {
+                var name = document.forms["LoginForm"]["Name"];
+                var password = document.forms["LoginForm"]["Password"];
+
+             if (name.value == "")                                  
+        { 
+            window.alert("Please enter your name."); 
+            name.focus(); 
+            return false; 
+        }
+
+         if (password.value == "")                        
+        { 
+            window.alert("Please enter your password"); 
+            password.focus(); 
+            return false; 
+        }
+                 return true;
+             }
+
+        </script>
+    </html>
